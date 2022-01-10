2e2a81a32314f93ae696cc3ec7b44a221886cae6
Uygar Karalalek <uy.kara@gmail.com>
2022-01-10 22:59:56 +0100

    Added right configuration to map request to index.html

--- Back-End/rest/src/main/kotlin/com/uygar/eshop/rest/AppConfiguration.kt
+++ Back-End/rest/src/main/kotlin/com/uygar/eshop/rest/AppConfiguration.kt
@@ -4,27 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.*
 import org.springframework.web.servlet.resource.PathResourceResolver
 
 @Configuration
-@EnableWebMvc
 class AppConfiguration : WebMvcConfigurer {
-
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*")
-    }
-    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry
-            .addResourceHandler("/fff/**")
-            .addResourceLocations("classpath:/static/")
-            .setCachePeriod(3600)
-            .resourceChain(true)
-            .addResolver(PathResourceResolver())
-    }
 
     @Bean
     fun swaggerConfiguration(): OpenAPI {
