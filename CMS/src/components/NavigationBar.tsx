ad6924da9cc624b683963d75ea81e5f139d2ed78
Uygar Karalalek <uy.kara@gmail.com>
2022-01-11 23:35:54 +0100

    Nothing functioning...

---
+++ CMS/src/components/NavigationBar.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export default class NavigationBar extends React.Component <{}> {
+
+    render() {
+        return (
+            <nav>d
+            </nav>
+        );
+    }
+
+}
--- CMS/src/index.tsx
+++ CMS/src/index.tsx
@@ -1,15 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import FirstComponent from './components/FirstComponent'
-import {UserComponent} from "./components/UserComponent";
-import {Example} from "./Example";
 
 ReactDOM.render(
     <div>
-        <h1>Hello, Welcome to React and TypeScript</h1>
         <FirstComponent/>
-        <Example/>
-        <UserComponent name="Uygar Karalalek" age={20} address="Via Gelusa 1" dob={new Date()}/>
     </div>,
     document.getElementById("root")
 );
