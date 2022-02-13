5b0a3c0944b8f15544734d3df8cacc8a19de44a4
uygar.karalalek <uy.kara@gmail.com>
2022-02-13 18:24:44 +0100

    Added SQL rows

--- Back-End/sql/inflate_table.sql
+++ Back-End/sql/inflate_table.sql
@@ -1,6 +1,12 @@
 INSERT INTO `e-shop`.shopping_card VALUES(1);
 INSERT INTO `e-shop`.user VALUES(1, 'Via Gelusa 1', 'uy.kara@gmail.com', 'Uygar', 'password', 'Karalalek', 1);
 INSERT INTO `e-shop`.product VALUES(1, 'Un must della filosofia', 30.05, 'La Formazione Filosofica');
+INSERT INTO `e-shop`.product VALUES(2, 'Compratelo se vuoi riuscire come matematico', 10.15, 'I numeri non sbagliano mai');
+INSERT INTO `e-shop`.product VALUES(3, 'Un romanzo russo imperdibile', 4.65, 'I fratelli Karamazov');
+INSERT INTO `e-shop`.product VALUES(4, 'Vai tu a speigare ad uno come questo cosa si prova durante le angoscie esistenziali', 5.9, 'Il male oscuro');
+INSERT INTO `e-shop`.product VALUES(5, 'Un must della poesia', 40, 'Poetica Aristotelica');
+INSERT INTO `e-shop`.product VALUES(6, 'Esposizioni matematiche niente male. Grorge Boole non appartiene ai nostri tempi, ma al futuro!', 30, 'Analisi matematica della logica');
+INSERT INTO `e-shop`.product VALUES(7, 'Java programmers', 12.3, 'Java Language Features');
 INSERT INTO `e-shop`.`order` VALUES(1, true, 1);
 INSERT INTO `e-shop`.order_product VALUES(1, 1, 1, 1);
 INSERT INTO `e-shop`.card_item VALUES(1, 1, date('01.01.2020'), 1);
